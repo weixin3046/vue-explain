b79d47fd144c0d9255a78298b836d98fbc97bc32
weixin4036 <408516111@qq.com>
2019-06-12 16:10:00 +0800

    配置文件的解读

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,8 +1,16 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
-//webpack-merge提供了一个合并函数，它将数组和合并对象创建一个新对象。
+//webpack-merge提供了一个合并函数，它将连接数组并合并对象。有点儿类似于es6的object.assign(); https://www.npmjs.com/package/webpack-merge
+//merge(
+//     {a : [1],b:5,c:20},
+//     {a : [2],b:10, d: 421}
+// )
+//合并后的结果
+//{a : [1,2] ,b :10 , c : 20, d : 421}
 //如果遇到函数，它将执行它们，通过算法运行结果，然后再次将返回的值封装在函数中.这边将dev和prod进行合并
+
+
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"'
 })
--- config/index.js
+++ config/index.js
@@ -8,25 +8,26 @@ module.exports = {
   dev: {
 
     // Paths
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    proxyTable: {},
+    assetsSubDirectory: 'static',//指的是静态资源文件夹，默认“static”，
+    assetsPublicPath: '/',//指的是发布路径，
+    proxyTable: {},//是我们常用来配置代理API的地方，
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: true,
-    errorOverlay: true,
-    notifyOnErrors: true,
+    autoOpenBrowser: true,//是否自动打开浏览器
+    errorOverlay: true,//查询错误
+    notifyOnErrors: true,//通知错误
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+    // poll是跟devserver相关的一个配置，webpack为我们提供的devserver是可以监控文件改动的，但在有些情况下却不能工作，我们可以设置一个轮询（poll）来解决
 
     // Use Eslint Loader?
     // If true, your code will be linted during bundling and
     // linting errors and warnings will be shown in the console.
-    useEslint: false,//是否显示警告
+    useEslint: false,//是否显示警告是否启用useEslint（代码检查）
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
-    showEslintErrorsInOverlay: false,
+    showEslintErrorsInOverlay: false,//是否展示eslint的错误提示
 
     /**
      * Source Maps
@@ -64,13 +65,13 @@ module.exports = {
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
-    productionGzipExtensions: ['js', 'css'],
+    productionGzip: false, //是否压缩，
+    productionGzipExtensions: ['js', 'css'],//模式下需要压缩的文件的扩展名，设置后会对相应扩展名的文件进行压缩
 
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
+    bundleAnalyzerReport: process.env.npm_config_report//是否开启打包后的分析报告
   }
 }
